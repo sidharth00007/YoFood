1ac7592bf918521f2caaba75db6cfb279a2ca50e
sidharth00007 <championsid07@gmail.com>
2023-08-01 17:35:32 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const DB_URL = process.env.DB_URL
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', `${DB_URL}`);
+  res.setHeader('Access-Control-Allow-Origin', `http://localhost:3000`);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
