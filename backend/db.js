a6008d7d8d3527615200f681d5a02d00881f87a2
sidharth00007 <championsid07@gmail.com>
2023-08-01 17:30:03 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+dotenv.config()
+// const DATABASE = process.env.DATABASE
 
-const mongoURL = 'mongodb+srv://gofood:02062002@cluster0.u2naj3h.mongodb.net/gofood?retryWrites=true&w=majority'
+const mongoURL = `mongodb+srv://gofood:${process.env.DB_PASS}@cluster0.u2naj3h.mongodb.net/gofood?retryWrites=true&w=majority`
 const mongoDB = async () => {
     await mongoose.connect(mongoURL, { useNewUrlParser: true }, async (err, result) => {
         if (err) console.log("---", err)
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port =  process.env.PORT || 5000
 const mongoDB = require('./db')
+const DB_URL = process.env.DB_URL
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', `${DB_URL}`);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
