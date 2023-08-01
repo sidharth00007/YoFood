e22f7a075be58461671a9cd527f61a4d299d51ce
sidharth00007 <championsid07@gmail.com>
2023-08-01 17:36:40 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port =  process.env.PORT || 5000
+const port = 5000
 const mongoDB = require('./db')
 const DB_URL = process.env.DB_URL
 mongoDB();
