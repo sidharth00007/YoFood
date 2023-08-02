9dfd4df837629ed5395c7b57e0529a6dfce2bbd1
sidharth00007 <championsid07@gmail.com>
2023-08-02 17:10:39 +0530

    made changes

--- src/baseURL.js
+++ src/baseURL.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "${BASE_URL}"
-// export const BASE_URL = "https://yofood.onrender.com" 
-export const BASE_URL = "http://localhost:5000" 
+export const BASE_URL = "https://yofood.onrender.com" 
+// export const BASE_URL = "http://localhost:5000" 
